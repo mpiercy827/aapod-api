68163e679365d4b30c61c7af1b48ef676b941a68
Matt Piercy <mpiercy@gmail.com>
2017-05-14 09:49:13 -0700

    feat(seed): Only seed db if media records are not present

--- test/libraries/dbSeeder.test.js
+++ test/libraries/dbSeeder.test.js
@@ -3,18 +3,33 @@
 const Sinon = require('sinon');
 const Bluebird = require('bluebird');
 
+const Knex     = require('../../db').Knex;
 const Download = require('../../src/libraries/download');
 const DBSeeder = require('../../src/libraries/dbSeeder');
 
+const DefaultMedia = {
+  date: '2017-05-07',
+  url: 'https://apod.nasa.gov/apod/image/1705/ic410_WISEantonucci_960.jpg',
+  type: 'image'
+};
+
 describe('DBSeeder', () => {
+
+  beforeEach(() => {
+    return Knex('media').truncate()
+    .then(() => {
+      return Knex('media').insert(DefaultMedia);
+    });
+  });
 
   describe('seed', () => {
 
-    it('seeds the database with all APOD media', () => {
+    it('seeds the database with all APOD media, unless already present', () => {
       const downloadStub = Sinon.stub(Download, 'download').returns(Bluebird.resolve({}));
 
       return DBSeeder.seed()
       .then(() => {
+        expect(downloadStub).to.not.have.been.calledWith('2017-05-07');
         expect(downloadStub).to.have.been.calledWith('2017-05-03');
         expect(downloadStub).to.have.been.calledWith('1995-06-20');
         expect(downloadStub).to.have.been.calledWith('1995-06-16');
